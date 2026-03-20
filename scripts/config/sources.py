0ecffa46a31318e17aa962da1e476d4a00171abd
peabarkley <peabarkley520@gmail.com>
2026-03-20 10:58:00 +0800

    Add source configuration for data collection
    Added configuration for data sources including Zhihu, Bilibili, and Baidu Tieba.

---
+++ scripts/config/sources.py
@@ -0,0 +1,18 @@
+# 采集源配置
+SOURCES = {
+    "知乎": {
+        "urls": [
+            "https://www.zhihu.com/topic/19554228/hot",
+            "https://www.zhihu.com/search?q=乌龟&type=content"
+        ]
+    },
+    "B站": {
+        "urls": [
+            "https://search.bilibili.com/all?keyword=乌龟",
+            "https://search.bilibili.com/all?keyword=养龟"
+        ]
+    },
+    "百度贴吧": {
+        "urls": ["https://tieba.baidu.com/f?kw=乌龟"]
+    }
+}

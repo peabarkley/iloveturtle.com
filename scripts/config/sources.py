ff337e445e887dcad330fc547588e1f528b3cfe6
peabarkley <peabarkley520@gmail.com>
2026-03-20 13:59:02 +0800

    Update sources.py

--- scripts/config/sources.py
+++ scripts/config/sources.py
@@ -1,27 +1,14 @@
-# 采集源配置 - 修正版（只采集乌龟相关话题）
+# 采集源配置 - 只保留确定有效的文章页面
 SOURCES = {
     "知乎": {
         "urls": [
-            "https://www.zhihu.com/search?q=乌龟&type=content",  # 知乎搜索
-            "https://www.zhihu.com/search?q=养龟&type=content",
-            "https://www.zhihu.com/search?q=巴西龟&type=content",
-            "https://www.zhihu.com/search?q=陆龟&type=content"
+            "https://zhuanlan.zhihu.com/p/615478392",  # 乌龟饲养指南
         ]
     },
     "B站": {
         "urls": [
-            "https://search.bilibili.com/all?keyword=乌龟饲养",  # B站搜索
-            "https://search.bilibili.com/all?keyword=龟病治疗",
-            "https://search.bilibili.com/all?keyword=龟缸造景",
-            "https://search.bilibili.com/all?keyword=陆龟",
-            "https://search.bilibili.com/all?keyword=蛋龟"
-        ]
-    },
-    "百度贴吧": {
-        "urls": [
-            "https://tieba.baidu.com/f?kw=乌龟",
-            "https://tieba.baidu.com/f?kw=巴西龟",
-            "https://tieba.baidu.com/f?kw=养龟"
+            "https://www.bilibili.com/video/BV1yz4y1D7JL",  # 乌龟饲养教程
+            "https://www.bilibili.com/video/BV1q54y1Q7wv",  # 巴西龟饲养
         ]
     }
 }
